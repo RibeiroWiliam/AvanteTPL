db426e7802abea36292841a24eb5d2fd9ff185e3
RibeiroWiliam <wiliamribeiro.tj@gmail.com>
2024-03-26 06:59:29 -0300

    Schema Update

--- avante-tpl/app/api/users/route.js
+++ avante-tpl/app/api/users/route.js
@@ -1,1 +1,18 @@
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const users = await prisma.user.findMany()
+    res.json(users)
+  } else if (req.method === 'POST') {
+    const { name, isAdmin, password } = req.body
+    const newUser = await prisma.user.create({
+      data: { name, isAdmin, password }
+    })
+    res.status(201).json(newUser)
+  } else {
+    res.status(405).json({ message: 'Method not allowed' })
+  }
+}
