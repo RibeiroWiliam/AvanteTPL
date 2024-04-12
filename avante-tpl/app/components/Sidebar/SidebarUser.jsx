9017ed0631db69df6af6b5acf6c3190037210e44
RibeiroWiliam <wiliamribeiro.tj@gmail.com>
2024-04-12 20:49:11 -0300

    SidebarUser bug fixes

--- avante-tpl/app/components/Sidebar/SidebarUser.jsx
+++ avante-tpl/app/components/Sidebar/SidebarUser.jsx
@@ -1,18 +1,19 @@
-import Image from "next/image"
+import Image from "next/image";
 
-export default function SidebarUser({user, isAdmin, pioneer, href}) {
+export default function SidebarUser({ user, isAdmin, pioneer, href }) {
   return (
     <a href={href}>
-      <div className={`p-2.5 mt-3 rounded-md px-4 duration-300 cursor-pointer text-white flex gap-4`}>
+      <div
+        className={`p-2.5 mt-3 rounded-md px-4 duration-300 cursor-pointer text-white flex gap-4`}
+      >
         <Image src="/userIcon.png" alt="User Icon" width={45} height={45} />
         <div className="flex flex-col text-start">
-        <span className="text-[16px] text-gray-200 font-bold">{user}</span>
-        <span className="text-[14px] text-gray-400 font-bold">{isAdmin ? "Admin" : ""}
-        {pioneer ? `Pioneiro ${pioneer}` : "Publicador"}
-        </span>
+          <span className="text-[16px] text-gray-200 font-bold">{user}</span>
+          <span className="text-[14px] text-gray-400 font-bold">
+            {isAdmin ? "Admin" : pioneer ? `Pioneiro ${pioneer}` : "Publicador"}
+          </span>
         </div>
-        
-      </div>   
+      </div>
     </a>
-  )
+  );
 }
