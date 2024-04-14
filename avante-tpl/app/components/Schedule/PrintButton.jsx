1f904359e54f7f2ea45cd436da0f9ab7e531d28a
RibeiroWiliam <wiliamribeiro.tj@gmail.com>
2024-04-14 15:34:36 -0300

    Print bug fixes

--- avante-tpl/app/components/Schedule/PrintButton.jsx
+++ avante-tpl/app/components/Schedule/PrintButton.jsx
@@ -1,4 +1,14 @@
-import { PDFDownloadLink } from "@react-pdf/renderer";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const PDFDownloadLink = dynamic(
+  () => import("@react-pdf/renderer").then((mod) => mod. PDFDownloadLink),
+  {
+    ssr: false,
+    loading: () => <p>Loading...</p>,
+  },
+);
 
 export default function PrintButton({document}) {
   return (
