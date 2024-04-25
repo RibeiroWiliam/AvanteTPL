17b20a62b679ba240530f238181dd8a50016e3ed
RibeiroWiliam <wiliamribeiro.tj@gmail.com>
2024-04-25 19:41:40 -0300

    Dashboard text

--- avante-tpl/app/(routes)/dashboard/page.jsx
+++ avante-tpl/app/(routes)/dashboard/page.jsx
@@ -1,16 +1,21 @@
-"use client"
+"use client";
 
 import Logo from "@/app/components/Shared/Logo";
 import { useSession } from "next-auth/react";
 
-
 export default function Dashboard() {
-  const {data: session} = useSession()
+  const { data: session } = useSession();
 
   return (
-    <div className="w-full flex justify-center items-center text-xl">
-      Olá <span className="ml-2 text-blue-600">{session && session.user.name}</span>!
-      Seja bem vindo ao <Logo theme="light"/>
+    <div className="w-full flex flex-col md:flex-row gap-1 justify-center items-center md:text-lg">
+      <div className="flex gap-1">
+        Olá
+        <span className="font-bold">{session && session.user.name}!</span>
+      </div>
+      <div className="flex items-center">
+        Seja bem vindo ao
+        <Logo theme="light" responsible />
+      </div>
     </div>
   );
 }
--- avante-tpl/app/components/Shared/Logo.jsx
+++ avante-tpl/app/components/Shared/Logo.jsx
@@ -5,15 +5,14 @@ const signika = Signika({
   subsets: ["latin"],
 });
 
-export default function Logo({theme, size}) {
+export default function Logo({theme, size, responsible=false}) {
   const textColor = theme === "light" ? "text-blue-800" : "text-white"
-
   const textSize = size ? size : "text-[25px]"
 
   return (
-    <div className={`${signika.className} ${textSize} flex px-2 font-bold uppercase gap-1`}>
-      <span className={`[text-shadow:_0_2px_0_rgb(0_0_0_/_40%)] ${textColor}`}>Avante</span>
-      <span className="[text-shadow:_0_2px_0_rgb(0_0_0_/_40%)] text-blue-600">Tpl</span>
+    <div className={`${signika.className} ${textSize} flex px-2 font-bold uppercase gap-1 text-xl`}>
+      <span className={`${textColor} ${responsible ? "sm:text-base md:text-lg" : ""}`}>Avante</span>
+      <span className={`text-blue-600 ${responsible ? "sm:text-base md:text-lg" : ""}`}>Tpl</span>
     </div>
   )
 }
