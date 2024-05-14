b9de344dbf5e6590031445432b93dec2076900cc
RibeiroWiliam <wiliamribeiro.tj@gmail.com>
2024-05-14 12:52:58 -0300

    Bug fixes

--- avante-tpl/app/(routes)/publishers/[id]/edit/page.jsx
+++ avante-tpl/app/(routes)/publishers/[id]/edit/page.jsx
@@ -3,16 +3,11 @@
 import { useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import axios from "axios";
-import { weekdays } from "@/app/constants/weekdays";
-import { shifts } from "@/app/constants/shifts";
-import { Availability } from "@/app/components/Availability";
-import { admin } from "@/app/middlewares/admin";
 import usePublisher from "@/app/hooks/usePublisher";
 import Title from "@/app/components/Shared/Title";
 
 export default function EditPublisher() {
   const router = useRouter();
-  admin();
   const { id } = useParams();
   const { publisher } = usePublisher(id);
   console.log(publisher);
@@ -21,60 +16,19 @@ export default function EditPublisher() {
     password: "",
     isAdmin: false,
     pioneer: "",
-    availabilities: [],
   });
-
-  const [menu, setMenu] = useState({
-    day: "Sat Mar 31 2024",
-    isOpen: false,
-    position: { x: 0, y: 0 },
-  });
-
-  const toggleMenu = () => {
-    setMenu((prevMenu) => ({
-      ...prevMenu,
-      isOpen: !prevMenu.isOpen,
-    }));
-  };
-
-  const handleCheckbox = (e) => {
-    const startTime = new Date(`${menu.day} ${e.target.value.startTime}`);
-    const endTime = new Date(`${menu.day} ${e.target.value.endTime}`);
-    const existingAvailability = formData.availabilities.filter(
-      (availability) => availability.startTime === startTime
-    );
-    if (existingAvailability) {
-      setFormData({
-        ...formData,
-        availabilities: formData.availabilities.filter(
-          (availability) => availability.startTime !== startTime
-        ),
-      });
-    } else {
-      setFormData({
-        ...formData,
-        availabilities: [
-          ...formData.availabilities,
-          {
-            startTime,
-            endTime,
-          },
-        ],
-      });
-    }
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(formData);
 
     if (formData.pioneer === "Nao") {
-      const { name, password, isAdmin, availabilities } = formData;
-      await setFormData({ name, password, isAdmin, availabilities });
+      const { name, password, isAdmin } = formData;
+      await setFormData({ name, password, isAdmin });
     }
 
     try {
-      const response = await axios.post("/api/publishers", formData);
+      const response = await axios.put(`/api/publishers/${id}`, formData);
       console.log(response.data); // Feedback para o usuário
       router.push("/publishers");
     } catch (error) {
@@ -148,79 +102,13 @@ export default function EditPublisher() {
                 </div>
               </div>
             </section>
-            <section>
-              <div className="flex gap-4 items-center">
-                <h2 className="text-2xl text-gray-700 font-bold ">
-                  Disponibilidades
-                </h2>
-                <button
-                  type="button"
-                  onClick={() => toggleMenu()}
-                  className="text-gray-600 hover:text-blue-600 text-2xl"
-                >
-                  <i className="bi bi-plus-circle"></i>
-                </button>
-              </div>
-              {formData.availabilities.map((availability) => (
-                <Availability.Card
-                  key={availability.startTime}
-                ></Availability.Card>
-              ))}
-            </section>
             <button
               type="submit"
-              className=" bottom-8 bg-blue-600 text-white px-4 py-2 hover:bg-blue-700 transition rounded-lg"
+              className=" bottom-8 bg-primary text-white px-4 py-2 hover:bg-hover transition duration-300 rounded-lg"
             >
-              Cadastrar
+              Salvar
             </button>
           </form>
-          {menu.isOpen && (
-            <form className="absolute mx-auto bg-white border border-gray-300 rounded-lg p-4 z-10 flex flex-col gap-4">
-              <button
-                type="button"
-                onClick={() => toggleMenu()}
-                className="absolute top-2 right-2 text-gray-800 hover:text-red-600"
-              >
-                <i className="bi bi-x-lg"></i>
-              </button>
-              <select
-                name=""
-                id=""
-                className="p-4 w-full text-center text-lg bg-white"
-              >
-                {weekdays.map((day) => (
-                  <option
-                    key={day.data}
-                    name="day"
-                    value={day.data}
-                    onChange={() => setMenu(...menu, day.data)}
-                  >
-                    {day.label}
-                  </option>
-                ))}
-              </select>
-              <div className="grid grid-cols-2 gap-4">
-                {shifts.map((shift) => (
-                  <div key={shift.startTime} className="flex gap-2">
-                    <input
-                      type="checkbox"
-                      onChange={handleCheckbox}
-                      name="shifts"
-                      id={shift.startTime}
-                      value={shift}
-                    />
-                    <label htmlFor={shift.startTime}>{shift.label}</label>
-                  </div>
-                ))}
-              </div>
-              {/* <button
-            type="submit"
-            className="bg-green-500 text-white px-4 py-2 w-full hover:bg-green-600 transition rounded-lg"
-          >
-            Salvar
-          </button> */}
-            </form>
-          )}
         </>
       )}
     </>
